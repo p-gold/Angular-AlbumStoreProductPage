95918a40496b5527595601266dee6b2cb02f69f6
p-gold <ochukwudi205@gmail.com>
2019-10-28 13:14:55 +0100

    --- Auto-Git Commit ---

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,13 @@ import { Album } from './album';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+ import { Product } from './product'
 
 
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
